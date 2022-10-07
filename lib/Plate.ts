edb6c7b20bf10531f510711bebc807faad567573
Achim Völz <achim.voelz.1991@gmail.com>
2022-10-07 18:58:26 +0200

    added refs and PlateDOM type

--- lib/Plate.ts
+++ lib/Plate.ts
@@ -1,44 +1,59 @@
-export function create(element: Element): any {
+export type PlateDOM = {
+    dom: any,
+    refs: any
+}
+
+export function create(element: Element): PlateDOM {
+    const refs = {};
+    const dom = createDOMAndFillRefs(element, refs);
+    return { dom, refs };
+}
+
+
+function createDOMAndFillRefs(element: Element, refs: any): any {
     const dom: any = {};
     for (let i = 0; i < element.children.length; i++) {
         const childElement: Element = element.children[i];
-        addElementToDOM(dom, childElement, childElement.tagName.toLowerCase());
+        const names = [childElement.tagName.toLowerCase()];
         const ref = childElement.getAttribute("ref");
         if (ref) {
-            addElementToDOM(dom, childElement, ref);
+            names.push(ref);
+        }
+
+        for (const name of names) {
+            let isArray = dom[name] instanceof Array;
+            if (dom[name]) {
+                isArray = true;
+                dom[name] = [dom[name]];
+            }
+
+            let child;
+            if (childElement.children.length === 0) {
+                child = childElement;
+            } else {
+                const childDOM = createDOMAndFillRefs(childElement, refs);
+                child = new Proxy({}, {
+                    get(_, prop) {
+                        if (prop === "self") {
+                            return childElement;
+                        }
+                        return childDOM[prop];
+                    }
+                });
+            }
+
+            if (isArray) {
+                dom[name].push(child);
+            } else {
+                dom[name] = child;
+            }
+
+            if (ref) {
+                refs[ref] = dom[name];
+            }
         }
     }
     return dom;
-}
-
-function addElementToDOM(dom: any, childElement: Element, name: string): void {
-    let isArray = dom[name] instanceof Array;
-    if (dom[name]) {
-        isArray = true;
-        dom[name] = [dom[name]];
-    }
-
-    let child;
-    if (childElement.children.length === 0) {
-        child = childElement;
-    } else {
-        const childDOM = create(childElement);
-        child = new Proxy({}, {
-            get(_, prop) {
-                if (prop === "self") {
-                    return childElement;
-                }
-                return childDOM[prop];
-            }
-        });
-    }
-
-    if (isArray) {
-        dom[name].push(child);
-    } else {
-        dom[name] = child;
-    }
-
 }
 
 export function text(element: Element, text: () => string): void {
@@ -59,8 +74,6 @@ export function attr(element: Element | Element[], name: string, value: () => Ob
 export function click(element: Element | Element[], handler: (e: Event) => void): void {
     updateForEach(element, el => el.addEventListener("click", handler));
 }
-
-
 
 function getElement(element: any) {
     if (element instanceof Element) {
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 
 import { attr, click, create, text } from "../lib/Plate";
 
-const dom = create(document.querySelector<HTMLElement>('#app')!);
+const plateDOM = create(document.querySelector<HTMLElement>('#app')!);
+const dom = plateDOM.dom;
 text(dom.h1, () => `Hello World!`);
 attr(dom.h1, "style", () => ({ padding: "10px", "background-color": "green", color: "white" }));
 attr(dom.div[0], "style", () => ({ padding: "10px", "background-color": "blue", color: "white" }));
 text(dom.div[0].h3, () => `Object!`);
-click(dom.div[1].button, (e) => {
+click(plateDOM.refs.helloBtn, (e) => {
   console.log(e);
   alert("hi!");
-})
+});
+click(dom.div[1].helloBtn, (e) => {
+  console.log(e);
+  alert("das!");
+});
