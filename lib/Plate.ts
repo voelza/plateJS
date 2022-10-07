51f32d4baa7180cd34715335d35cac30763ff8f9
Achim Völz <achim.voelz.1991@gmail.com>
2022-10-07 18:46:54 +0200

    ref and element-name in plate dom both added

--- lib/Plate.ts
+++ lib/Plate.ts
@@ -2,37 +2,43 @@ export function create(element: Element): any {
     const dom: any = {};
     for (let i = 0; i < element.children.length; i++) {
         const childElement: Element = element.children[i];
-
-        const name = childElement.getAttribute("ref") ?? childElement.tagName.toLowerCase();
-
-        let isArray = dom[name] instanceof Array;
-        if (dom[name]) {
-            isArray = true;
-            dom[name] = [dom[name]];
-        }
-
-        let child;
-        if (childElement.children.length === 0) {
-            child = childElement;
-        } else {
-            const childDOM = create(childElement);
-            child = new Proxy({}, {
-                get(_, prop) {
-                    if (prop === "self") {
-                        return childElement;
-                    }
-                    return childDOM[prop];
-                }
-            });
-        }
-
-        if (isArray) {
-            dom[name].push(child);
-        } else {
-            dom[name] = child;
+        addElementToDOM(dom, childElement, childElement.tagName.toLowerCase());
+        const ref = childElement.getAttribute("ref");
+        if (ref) {
+            addElementToDOM(dom, childElement, ref);
         }
     }
     return dom;
+}
+
+function addElementToDOM(dom: any, childElement: Element, name: string): void {
+    let isArray = dom[name] instanceof Array;
+    if (dom[name]) {
+        isArray = true;
+        dom[name] = [dom[name]];
+    }
+
+    let child;
+    if (childElement.children.length === 0) {
+        child = childElement;
+    } else {
+        const childDOM = create(childElement);
+        child = new Proxy({}, {
+            get(_, prop) {
+                if (prop === "self") {
+                    return childElement;
+                }
+                return childDOM[prop];
+            }
+        });
+    }
+
+    if (isArray) {
+        dom[name].push(child);
+    } else {
+        dom[name] = child;
+    }
+
 }
 
 export function text(element: Element, text: () => string): void {
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ text(dom.h1, () => `Hello World!`);
 attr(dom.h1, "style", () => ({ padding: "10px", "background-color": "green", color: "white" }));
 attr(dom.div[0], "style", () => ({ padding: "10px", "background-color": "blue", color: "white" }));
 text(dom.div[0].h3, () => `Object!`);
-click(dom.div[1].helloBtn, (e) => {
+click(dom.div[1].button, (e) => {
   console.log(e);
   alert("hi!");
 })
